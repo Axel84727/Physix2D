8304a6bbbd709e363df624fe0bf78fb959977611
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-11-23 23:58:53 -0300

    feat: Implement vec2 struct with default and parameterized constructors.

---
+++ include/vec2.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+struct vec2
+{
+    float x;
+    float y;
+
+    vec2();
+
+    vec2(float x_val, float y_val);
+};
---
+++ src/vec2.cpp
@@ -0,0 +1,9 @@
+#include "vec2.hpp"
+
+vec2::vec2() : x(0.0f), y(0.0f)
+{
+}
+
+vec2::vec2(float val_x, float val_y) : x(val_x), y(val_y)
+{
+}
